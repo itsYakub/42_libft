6b453958d198d3d7b49e94a83910c7a820105df3
Jakub Oleksiak <joleksia@c3r1s4.42warsaw.pl>
2024-12-07 08:40:59 +0100

    Norminette styling for libft.h

--- lift.h
+++ lift.h
@@ -6,34 +6,34 @@
 /*   By: joleksia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 11:34:10 by joleksia          #+#    #+#             */
-/*   Updated: 2024/12/02 11:57:21 by joleksia         ###   ########.fr       */
+/*   Updated: 2024/12/07 08:40:35 by joleksia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* SECTION: Inclusions */
 
 /* SECTION: Function definitions */
-int isalpha(int c);
-int isdigit(int c);
-int isalnum(int c);
-int isascii(int c);
-int isprint(int c);
-int strlen(const char* s);
-void *memset(void *dest, int ch, size_t count);
-void *bzeor(void* ptr, size_t n);
-void *memcpy(void *dest, const void *src, size_t count);
-void *memmove(void* dest, const void* src, size_t count);
-char *strncpy(char *dest, const char *src, size_t count);
-size_t strlcpy(char *dst, const char *src, size_t size);
-size_t strlcat(char *dst, const char *src, size_t size);
-int toupper(int c);
-int tolower(int c);
-char *strchr(const char *s, int c);
-char *strrchr(const char *s, int c);
-int strncmp(const char *s1, const char *s2, size_t n);
-void *memchr(const void *s, int c, size_t n);
-int memcmp(const void *s1, const void *s2, size_t n);
-char *strnstr(const char *big, const char *little, size_t len);
-int atoi(const char *nptr);
-void *malloc(size_t size);
-char *strdup(const char *s);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(const char *s);
+void	*ft_memset(void *dest, int ch, size_t count);
+void	*ft_bzeor(void *ptr, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t count);
+void	*ft_memmove(void *dest, const void *src, size_t count);
+char	*ft_strncpy(char *dest, const char *src, size_t count);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
+void	ft_malloc(size_t size);
+char	*ft_strdup(const char *s);
